937026248aaec7759fb3afecb088cabe6ae16106
Amar2502 <amarpandey2502@gmail.com>
2026-06-30 11:40:33 +0530

    server and db connection setup

---
+++ backend/src/server.ts
@@ -0,0 +1,21 @@
+import app from "./app";
+import { config } from "./config/config";
+import { prisma } from "./config/db";
+
+const PORT = config.PORT;
+
+async function start_server() {
+    try {
+        await prisma.$connect();
+        console.log("Database connected successfully");
+        
+        app.listen(PORT, ()=> {
+            console.log(`Server is running on port ${PORT}`);
+        })
+    }
+    catch (error) {
+        console.error("Database connection failed:", error);
+    }
+}
+
+start_server()
